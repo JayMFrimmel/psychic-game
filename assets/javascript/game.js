28683f4175cf74e62427dadb7ee7eb29196438b7
JayMFrimmel <jayfrimmel@yahoo.com>
2016-10-23 22:36:45 -0400

    added code

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+//set up variables
+var letterGuess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// prompt the user
+var userGuess = prompt("Guess a letter!");
+
+
+//convert to lower case
+var userGuessLower = userGuess.toLowerCase();
+
+//check against the letter chosen by the computer
+for 
+}
